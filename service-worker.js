1d37e6226f4d8de3846648dbca391c0d47016dd7
PatriciaRodrigues94 <dacosta_rodrigues@hotmail.com>
2026-01-31 17:33:00 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "app-fitness-2026-01-31-v5";
+const CACHE_NAME = "app-fitness-2026-01-31-v6";
 const ASSETS = [
   "./",
   "./index.html",
@@ -32,6 +32,7 @@ self.addEventListener("fetch", (event) => {
   );
 
 });
+
 
 
 
